e24e582b18a85e617b1c2d881e41b469861508a8
Paulo Filho <paulomacieltorresfilho@gmail.com>
2023-03-12 09:54:52 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let containerEl = document.getElementsByClassName("container")[0];
+let elementoBase = document.getElementById("first");
+const quantidadeElementos = 20; //90
+
+
+for (let i = 0; i < quantidadeElementos - 1; i++) {
+    let copy = elementoBase.cloneNode(true);
+    containerEl.appendChild(copy);
+}
